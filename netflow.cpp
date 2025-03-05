4a75f1e9df42e5b20f6f81169e9bd428f1baaa22
GavinITP <86824319+GavinITP@users.noreply.github.com>
2025-03-05 21:40:25 +0700

    feat: creat snmp flow

--- netflow.cpp
+++ netflow.cpp
@@ -347,6 +347,22 @@ NetflowPayload createFtpFlow()
     return payload;
 }
 
+NetflowPayload createSnmpFlow()
+{
+    NetflowPayload payload;
+
+    payload.srcIp = ipToUint32("112.10.20.10");
+    payload.dstIp = ipToUint32("172.30.190.10");
+    payload.nextHopIp = ipToUint32("172.199.15.1");
+    payload.srcPort = static_cast<uint16_t>(40);
+    payload.dstPort = static_cast<uint16_t>(SNMP_PORT);
+    payload.snmpInIndex = static_cast<uint16_t>(rand() % 2);
+
+    fillCommonFields(payload, PAYLOAD_AVG_MD, 17, rand() % 32);
+
+    return payload;
+}
+
 NetflowPayload createRandomFlow()
 {
     NetflowPayload payload;
